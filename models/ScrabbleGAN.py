abd48b0fa1a13aa7e3e75001d1163fc6793fd4a0
Nicholas Robertson <nick@the-dream-machine.local>
2020-11-27 11:13:07 +0100

    slack hint

--- models/ScrabbleGAN.py
+++ models/ScrabbleGAN.py
@@ -263,3 +263,6 @@ class ScrabbleGAN(pl.LightningModule):
         if optimizer_idx == 1:
             optimizer.step()
             optimizer.zero_grad()
+    @property
+    def automatic_optimization() -> bool:
+        return False
